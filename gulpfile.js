83ecbf02b73670365ad98c605b17abf1f25e8cd0
Jakub Babrzymąka <jakub.babrzymaka@gmail.com>
2016-03-28 01:47:25 +0200

    90%

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp=require('gulp');
 var uglify=require('gulp-uglify');
 var sass=require('gulp-ruby-sass');
 var autoprefixer=require('gulp-autoprefixer');
+var livereload=require('gulp-livereload');
 //uglify
 gulp.task('scripts', function(){
 	gulp.src('js/effects.js').pipe(uglify()).pipe(gulp.dest('build/js/'));
@@ -14,11 +15,12 @@ gulp.task('styles',function(){
 
 //autoprefixer
 gulp.task('autoprefix', function(){
-	gulp.src('build/css/*.css').pipe(autoprefixer()).pipe(gulp.dest('css/'));
+	gulp.src('build/css/*.css').pipe(autoprefixer()).pipe(gulp.dest('css/')).pipe(livereload());
 });
 
 //watch
 gulp.task('watch', function(){
+	var server=livereload();
 	gulp.watch('js/effects.js', ['scripts']);
 	gulp.watch('sass/*.scss', ['styles']);
 	gulp.watch('build/css/*.css', ['autoprefix']);
